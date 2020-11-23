a5505e4b68224bd05dd3b5d7480fa7882a7a4f36
Mathew <mwakre29@gmail.com>
2020-11-23 14:32:25 -0800

    import landingpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
 
+import LandingPage from './components/LandingPage/LandingPage';
+
 function App() {
   return (
     <div>
-   
+      <LandingPage/>
     </div>
   );
 }
