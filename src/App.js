c42df921821a2e68c5b6739c55714633207eb679
Mathew <mwakre29@gmail.com>
2020-11-23 14:27:43 -0800

    Clean up file

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import styles from './App.module.css';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   
     </div>
   );
 }
