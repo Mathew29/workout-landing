24bf5e29d354281de816fa3774cbaa85c0f3f952
Mathew <mwakre29@gmail.com>
2020-11-24 13:51:31 -0800

    import asset

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import workoutPic from '../../assets/images/karsten-winegeart-0Wra5YYVQJE-unsplash.jpg';
 
 const LandingPage = () => {
     
     return (
         <div>
             <h1>Hello World</h1>
+            <img src={workoutPic} alt="workout pic" />
         </div>
     )
 }
