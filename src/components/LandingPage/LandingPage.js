27b086f142783b42e0d6ce1544b6f31d7fe28ab9
Mathew <mwakre29@gmail.com>
2020-11-30 14:01:49 -0800

    move backgroun image

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
 import styles from "./LandingPage.module.css"
-import workoutPic from '../../assets/images/karsten-winegeart-0Wra5YYVQJE-unsplash.jpg';
 
 const LandingPage = () => {
     
     return (
-        <div>
-            <img className={styles.Workout} src={workoutPic} alt="workout pic" />
+        <div >
+            <h1>HELLOOOOOO</h1>
         </div>
     )
 }
