77e1d576b88fe3165f3e3983357820b5a2e54ead
Mathew <mwakre29@gmail.com>
2020-11-30 16:02:34 -0800

    revert background image and dealing with modal issues

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,8 +5,12 @@ import styles from "./LandingPage.module.css"
 const LandingPage = () => {
     
     return (
-        <div >
-            <h1>HELLOOOOOO</h1>
+        <div className={styles.Container}>
+            <div className={styles.Modal}>
+                <input style={styles.Input} placeholder="First Name..." className={styles.FirstName} type="string" />
+                <input style={styles.Input} placeholder="Last Name..." className={styles.LastName} type="string" />
+                <input style={styles.Input} placeholder="Email" className={styles.Email} type="string" />
+            </div>
         </div>
     )
 }
