279a75859e44015039c255b0b5f5c653857c82ae
Mathew <mwakre29@gmail.com>
2020-12-02 13:02:29 -0800

    working on modal

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,25 +6,27 @@ const LandingPage = () => {
   return (
     <div className={styles.Background}>
       <div className={styles.Container}>
-        <div className={styles.Modal}>
-          <input
-            style={{marginTop: '10px'}}
-            placeholder="First Name..."
-            className={styles.FirstName}
-            type="string"
-          />
-          <input
-            style={{marginTop: '10px'}}
-            placeholder="Last Name..."
-            className={styles.LastName}
-            type="string"
-          />
-          <input
-            style={{marginTop: '10px'}}
-            placeholder="Email"
-            className={styles.Email}
-            type="string"
-          />
+        <div className={styles.ModalContainer}>
+          <div className={styles.Modal}>
+            <input
+              style={{ marginTop: "10px" }}
+              placeholder="First Name..."
+              className={styles.FirstName}
+              type="string"
+            />
+            <input
+              style={{ marginTop: "10px" }}
+              placeholder="Last Name..."
+              className={styles.LastName}
+              type="string"
+            />
+            <input
+              style={{ marginTop: "10px" }}
+              placeholder="Email"
+              className={styles.Email}
+              type="string"
+            />
+          </div>
         </div>
       </div>
     </div>
