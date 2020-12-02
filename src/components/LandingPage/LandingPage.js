7560e54afe792f6fc1021ac114a37820c7fd102a
Mathew <mwakre29@gmail.com>
2020-12-02 12:10:41 -0800

    Move modal to left center screen

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React from "react";
 
-import styles from "./LandingPage.module.css"
+import styles from "./LandingPage.module.css";
 
 const LandingPage = () => {
-    
-    return (
-        <div className={styles.Container}>
-            <div className={styles.Modal}>
-                <input style={styles.Input} placeholder="First Name..." className={styles.FirstName} type="string" />
-                <input style={styles.Input} placeholder="Last Name..." className={styles.LastName} type="string" />
-                <input style={styles.Input} placeholder="Email" className={styles.Email} type="string" />
-            </div>
+  return (
+    <div className={styles.Background}>
+      <div className={styles.Container}>
+        <div className={styles.Modal}>
+          <input
+            style={{marginTop: '10px'}}
+            placeholder="First Name..."
+            className={styles.FirstName}
+            type="string"
+          />
+          <input
+            style={{marginTop: '10px'}}
+            placeholder="Last Name..."
+            className={styles.LastName}
+            type="string"
+          />
+          <input
+            style={{marginTop: '10px'}}
+            placeholder="Email"
+            className={styles.Email}
+            type="string"
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
-
-
