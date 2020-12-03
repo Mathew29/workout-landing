4f09cbca2514ffe95f2ed2061a892befa12865ff
Mathew <mwakre29@gmail.com>
2020-12-03 12:51:26 -0800

    style input fields to be responsive

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -9,19 +9,43 @@ const LandingPage = () => {
         <div className={styles.ModalContainer}>
           <div className={styles.Modal}>
             <input
-              style={{ marginTop: "10px" }}
+              style={{
+                marginTop: "10px",
+                width: "100%",
+                padding: "5px",
+                border: "1px, solid, black",
+                WebkitBoxSizing: "border-box",
+                MozBoxSizing: "border-box",
+                boxSizing: "border-box",
+              }}
               placeholder="First Name..."
               className={styles.FirstName}
               type="string"
             />
             <input
-              style={{ marginTop: "10px" }}
+              style={{
+                marginTop: "10px",
+                width: "100%",
+                padding: "5px",
+                border: "1px, solid, black",
+                WebkitBoxSizing: "border-box",
+                MozBoxSizing: "border-box",
+                boxSizing: "border-box",
+              }}
               placeholder="Last Name..."
               className={styles.LastName}
               type="string"
             />
             <input
-              style={{ marginTop: "10px" }}
+              style={{
+                marginTop: "10px",
+                width: "100%",
+                padding: "5px",
+                border: "1px, solid, black",
+                WebkitBoxSizing: "border-box",
+                MozBoxSizing: "border-box",
+                boxSizing: "border-box",
+              }}
               placeholder="Email"
               className={styles.Email}
               type="string"
